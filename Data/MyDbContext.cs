66887928b9a7d8baca8ce1526b3e0098543a188a
AyomidePeggie <Shobandedeborah@gmail.com>
2025-02-15 19:40:11 +0100

    Started Authorization and Authentication

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyFirstApp.Entities;
+using MyFirstApp.Entities.Identity;
 
 namespace MyFirstApp.Data
 {
-	public class MyDbContext: DbContext
+	public class MyDbContext: IdentityDbContext <ApplicationUser>
 	{
         public MyDbContext(DbContextOptions<MyDbContext> options): base(options) 
         {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MyFirstApp.Data;
+using MyFirstApp.Entities.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var services = builder.Services;
 services.AddDbContext<MyDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars=1;
+
+    options.SignIn.RequireConfirmedEmail = false;
+
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromHours(1);
+    options.Lockout.AllowedForNewUsers = false;
+});
+services.AddIdentity<ApplicationUser, IdentityRole>(options=>options
+.SignIn.RequireConfirmedAccount=false).AddEntityFrameworkStores<MyDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +36,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
