f1fa90726d2f128ca453b15d01588f642b06e0e9
AyomidePeggie <Shobandedeborah@gmail.com>
2025-02-20 19:41:23 +0100

    Account authentication

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstApp.Entities.Identity;
+using MyFirstApp.Models;
+
+namespace MyFirstApp.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public AccountController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+		{
+			_roleManager = roleManager;
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginModel loginmodel)
+		{
+			var user = await _userManager.FindByNameAsync(loginmodel.Username);
+			if (user == null) 
+			return View(loginmodel);
+
+			//try to sign the user in with the credentials they supplied
+
+			var result = await _signInManager.PasswordSignInAsync(loginmodel.Username
+				,loginmodel.Password, false,true);
+			if (result.Succeeded)
+			{
+				return View(loginmodel);
+			}
+			else
+			{
+				ModelState.AddModelError("", "Invalid username or password");
+				return View(loginmodel);
+			}
+		}
+	}
+
+	
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstApp.Models
+{
+	public class LoginModel
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
