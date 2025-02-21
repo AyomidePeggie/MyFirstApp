294a961d354579ca5090964f0ec3049004d58bdf
AyomidePeggie <Shobandedeborah@gmail.com>
2025-02-21 19:35:38 +0100

    Login implementation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MyFirstApp.Entities.Identity;
 using MyFirstApp.Models;
+using System.Security.Claims;
 
 namespace MyFirstApp.Controllers
 {
@@ -40,13 +41,44 @@ namespace MyFirstApp.Controllers
 				,loginmodel.Password, false,true);
 			if (result.Succeeded)
 			{
-				return View(loginmodel);
+				var firstNameClaim =new Claim("Firstname", user.FirstName);
+				var lastNameClaim = new Claim ("Lastname", user.LastName);
+				var dateOfBirthClaim = new Claim(ClaimTypes.DateOfBirth, "2001-10-01");
+
+				await _userManager.AddClaimAsync(user, firstNameClaim);
+				await _userManager.AddClaimAsync(user, lastNameClaim);
+				await _userManager.AddClaimAsync(user, dateOfBirthClaim);
+
+				await _signInManager.RefreshSignInAsync(user);
+
+				var roles = await _userManager.GetRolesAsync(user);
+
+				if (roles.Any(x => x == "Admin"))
+				{
+					return Redirect(Url.Action("Index", "Admin"));
+				}
+				else if (roles.Any(x => x == "Visitor"))
+				{
+					return Redirect(Url.Action("Index", "Home"));
+				}
+				return Redirect(GetRedirectUrl("/"));
+
+			
 			}
 			else
 			{
 				ModelState.AddModelError("", "Invalid username or password");
 				return View(loginmodel);
 			}
+		}
+
+		private string GetRedirectUrl(string returnUrl)
+		{
+			if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+			{
+				return Url.Action("Index", "Home");
+			}
+			return returnUrl;
 		}
 	}
 
--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MyFirstApp.Entities;
 using MyFirstApp.Entities.Identity;
@@ -24,6 +25,32 @@ namespace MyFirstApp.Data
 				.WithMany(c => c.Products) // category has many products 
 				.HasForeignKey(p => p.CategoryId) //foreign key 
 				.OnDelete(DeleteBehavior.Cascade);
+			var hasher = new PasswordHasher<ApplicationUser>();
+
+			//create a role
+			modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7210", Name = "Admin", NormalizedName = "ADMIN" });
+
+			//create a user
+			modelBuilder.Entity<ApplicationUser>().HasData(
+			   new ApplicationUser
+			   {
+				   Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+				   UserName = "ecommerceadmin",
+				   NormalizedUserName = "ECOMMERCEADMIN",
+				   PasswordHash = hasher.HashPassword(null, "Pa$$w0rd"),
+				   FirstName = "Admin",
+				   LastName = "Admin"
+
+			   }
+			   );
+
+			//asign admin role to the user we created
+			modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+			{
+				RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+				UserId = "8e445865-a24d-4543-a6c6-9443d048cdb9"
+
+			});
 
 			base.OnModelCreating(modelBuilder);
 		}
